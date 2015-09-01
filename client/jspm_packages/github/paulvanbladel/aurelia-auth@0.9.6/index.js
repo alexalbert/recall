899ab829b0c7f34727e6c8468e47bab08f7e20cb
alexalbert <albalex@gmail.com>
2015-09-01 11:18:07 -0400

    chore: bundling

---
+++ client/jspm_packages/github/paulvanbladel/aurelia-auth@0.9.6/index.js
@@ -0,0 +1,30 @@
+/* */ 
+System.register(['./baseConfig', './authService', './authorizeStep'], function (_export) {
+	'use strict';
+
+	var BaseConfig;
+
+	_export('configure', configure);
+
+	function configure(aurelia, configCallback) {
+		aurelia.globalizeResources('./authFilter');
+
+		var baseConfig = aurelia.container.get(BaseConfig);
+		if (configCallback !== undefined && typeof configCallback === 'function') {
+			configCallback(baseConfig);
+		}
+	}
+
+	return {
+		setters: [function (_baseConfig) {
+			BaseConfig = _baseConfig.BaseConfig;
+		}, function (_authService) {
+			_export('AuthService', _authService.AuthService);
+		}, function (_authorizeStep) {
+			_export('AuthorizeStep', _authorizeStep.AuthorizeStep);
+		}],
+		execute: function () {
+			;
+		}
+	};
+});
